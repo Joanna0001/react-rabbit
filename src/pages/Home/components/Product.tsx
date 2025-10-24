5233579d6bd1e2eca1ad9e316db8cdab36ddb884
Joanna0001 <gkj159@163.com>
2025-10-24 09:14:19 +0800

    feat: product

--- src/pages/Home/components/Product.tsx
+++ src/pages/Home/components/Product.tsx
@@ -1,13 +1,12 @@
 import { useGoodsListQuery } from '@/hooks/useHome';
 import type { GoodsResponse } from '@/types/category';
-import { Typography } from 'antd';
-
-const { Title } = Typography;
+import { Image, Flex } from 'antd';
+import { ProductCard } from '@/components/ProductCard';
 
 export function Product() {
   const { data: goodsList } = useGoodsListQuery();
   return (
-    <div className="bg-white" style={{ padding: '0 calc(50% - var(--max-width)/2)', marginTop: 20 }}>
+    <div className="bg-white my-5" style={{ padding: '0 calc(50% - var(--max-width)/2)' }}>
       {goodsList?.map(item => (
         <ProductItem key={item.id} item={item} />
       ))}
@@ -18,8 +17,17 @@ export function Product() {
 function ProductItem({ item }: { item: GoodsResponse }) {
   return (
     <div>
-      <Title level={5}>{item.name}</Title>
-      <div></div>
+      <h3 className="text-[32px] ml-[8px] py-10">{item.name}</h3>
+      <Flex>
+        <div>
+          <Image preview={false} src={item.picture} width={240} height={610} />
+        </div>
+        <div className="ml-[20px] flex flex-wrap gap-[20px]">
+          {item.goods.map(good => (
+            <ProductCard key={good.id} {...good} />
+          ))}
+        </div>
+      </Flex>
     </div>
   );
 }
